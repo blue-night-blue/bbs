74b534c332248ed1a7a9aa59b7957f1b1614391c
bnb <deathproooof@gmail.com>
2023-05-20 11:46:50 +0900

    add function of page but mikansei

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,93 @@
+class HomeController < ApplicationController
+
+  def top
+    @post=Post.all.order(created_at: :desc)
+  end
+
+
+
+
+  def create
+    @post=Post.new(
+      name:params[:name],
+      content:params[:content],
+      password:params[:password]
+      ) 
+
+    if @post.name ==""
+      @post.name = "名無し"
+    end
+
+    if params[:password] ==""
+      @post.password = "asheifjrewrjewajrawehfwehurhu4"
+    end
+    
+    @post.save
+    flash[:notice]="投稿しました"
+    redirect_to ("/")
+    
+  end
+
+
+
+  def edit
+    @post=Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post=Post.find_by(id: params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+
+    if @post.authenticate(params[:password])
+      @post.save
+      flash[:notice]="修正しました"
+      redirect_to ("/")
+    else
+      @error_message = "パスワードが間違っています" 
+      render ("home/edit")
+    end
+  end
+
+
+
+
+
+  def delete
+    @post=Post.find_by(id: params[:id])
+  end
+
+  def destroy
+    @post=Post.find_by(id: params[:id])
+
+    if @post.authenticate(params[:password])
+      @post.destroy
+      flash[:notice]="削除しました"
+      redirect_to ("/")
+    else
+      @error_message = "パスワードが間違っています" #うまくいかない。なぜ？
+      render ("home/delete")
+    end
+  end
+
+
+
+
+
+  def page
+    @page=params[:id].to_i
+    number_from=(@page-1)*10+1
+    number_to=number_from+9
+
+    @post=Post.where(id:number_from..number_to).order(created_at: :desc)
+  end
+
+
+
+
+
+
+  
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,33 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root 'home#top'
+  get "/" => 'home#top'
+
+  post "home/create" => 'home#create'
+  
+  get ":id/edit" => 'home#edit'
+  post ":id/update" => 'home#update'
+
+  get ":id/delete" => 'home#delete'
+  post ":id/destroy" => 'home#destroy'
+
+  get "page/1" => 'home#top'
+  get "page/:id" => 'home#page'
+  
+  
+  
+  
+
+
+
+
+  get "test1" => 'home#test1'
+  post "update2/:id" => 'home#test2'
+
+
+
+end
